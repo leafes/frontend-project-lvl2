9b8dfc8706d6ffe0c58c6c559f0f7e21b837515d
leafes <xmm.geek@gmail.com>
2022-07-20 20:30:59 +0900

    add -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,11 @@ import { program } from 'commander';
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-v, --vers', 'output the current version');
+    .version('0.0.1', '-v, --vers', 'output the current version')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
 
+program
+    .option('-f, --format <type>', 'output format');
+    
 program.parse();
