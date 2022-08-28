90c367fed61a5959156e5450ef3b7d9778965555
leafes <xmm.geek@gmail.com>
2022-08-28 18:56:04 +0900

    changed default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.2', '-v, --vers', 'output the current version')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format tree|plain|json', 'json')
+  .option('-f, --format [type]', 'output format tree|plain|json', 'tree')
   .action((filepath1, filepath2, options) => {
     const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
