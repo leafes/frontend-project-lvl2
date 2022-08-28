cb7f776b9d1c3bbc9030cfadb62b1c62c3223639
leafes <xmm.geek@gmail.com>
2022-08-28 17:31:23 +0900

    add formatters

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,18 @@
 #! node
 
 import { program } from 'commander';
-import genDiff from '../src/gendiff.js';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v, --vers', 'output the current version')
+  .version('1.0.2', '-v, --vers', 'output the current version')
+  .option('-f, --format <type>', 'output format [tree, plain, json]', 'json')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => console.log(`{\n${genDiff(filepath1, filepath2)}\n}`));
-
-program
-  .option('-f, --format <type>', 'output format');
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2, program.format);
+    console.log(diff);
+  });
 
 program.parse();
