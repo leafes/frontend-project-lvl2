6730f34550575fcbe71ce11363d90b35208cab5f
leafes <xmm.geek@gmail.com>
2022-08-30 01:33:28 +0900

    change variables to const

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,15 +4,9 @@ import gendiff from '../src/index.js';
 const testFilesPath = '__fixtures__/';
 const expans = ['json', 'yml'];
 
-let resultStylish;
-let resultPlain;
-let resultJson;
-
-beforeEach(() => {
-  resultStylish = fs.readFileSync(`${testFilesPath}result_tree.txt`, 'utf-8');
-  resultPlain = fs.readFileSync(`${testFilesPath}result_plain.txt`, 'utf-8');
-  resultJson = fs.readFileSync(`${testFilesPath}result_json.txt`, 'utf-8');
-});
+const resultStylish = fs.readFileSync(`${testFilesPath}result_tree.txt`, 'utf-8');
+const resultPlain = fs.readFileSync(`${testFilesPath}result_plain.txt`, 'utf-8');
+const resultJson = fs.readFileSync(`${testFilesPath}result_json.txt`, 'utf-8');
 
 describe.each(expans)('gendiff %s', (exp) => {
   const before = `${testFilesPath}file1.${exp}`;
