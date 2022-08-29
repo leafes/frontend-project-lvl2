f763f93dd142e0c51b2909a4d919a810439e0d7e
leafes <xmm.geek@gmail.com>
2022-08-30 01:42:45 +0900

    fix const

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,6 +12,9 @@ describe.each(expans)('gendiff %s', (exp) => {
   const before = `${testFilesPath}file1.${exp}`;
   const after = `${testFilesPath}file2.${exp}`;
   test('gendiff', () => {
+    const resultStylish = fs.readFileSync(`${testFilesPath}result_tree.txt`, 'utf-8');
+    const resultPlain = fs.readFileSync(`${testFilesPath}result_plain.txt`, 'utf-8');
+    const resultJson = fs.readFileSync(`${testFilesPath}result_json.txt`, 'utf-8');
     expect(gendiff(before, after, 'stylish')).toEqual(resultStylish.trim());
     expect(gendiff(before, after, 'plain')).toEqual(resultPlain.trim());
     expect(gendiff(before, after, 'json')).toEqual(resultJson.trim());
